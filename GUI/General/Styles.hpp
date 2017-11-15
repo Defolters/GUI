fd0263aa21a432c1c21bf79a1784ad4a9f11493f
IgorBeschastnov <30983257+IgorBeschastnov@users.noreply.github.com>
2017-11-15 17:50:19 +0300

    updae

--- GUI/General/Styles.hpp
+++ GUI/General/Styles.hpp
@@ -7,11 +7,12 @@ struct TextStyle {
 	Font font;
 	int fontSize;
 	Color color;
-	char align;
+	char align;//единственное интересное место. Если == 'c', то текст равнятеся по центру, елси 'l' - по левому краю
 };
+
 //Стиль для текста - шрифт, размер и цвет
 struct GUIStyle {
-	Texture mainTex, pressTex, overTex;
-	Texture frame;
-	int frameWid;
+	Texture mainTex, pressTex, overTex;//пока что не робит, грузите все одной текстурой
+	Texture frame;//рамки (в тестовом примере просто коричневые)
+	int frameWid;//ширина рамки в пикселях
 };
