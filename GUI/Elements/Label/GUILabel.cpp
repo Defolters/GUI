afc84ff46bdb4084682b68557a5ccc0ca6291605
Daniel Emelyanov <defolterrik@gmail.com>
2017-11-18 23:18:20 +0300

    GUILabel

--- GUI/Elements/Label/GUILabel.cpp
+++ GUI/Elements/Label/GUILabel.cpp
@@ -4,8 +4,6 @@ GUILabel::GUILabel(RenderWindow* renderWindow_, float x, float y, float width,
     float height, std::string text_, TextStyle *Ctstyle, GUIStyle *Cgstyle) : 
     GUIBox(renderWindow_, x, y, width, height, text_, Ctstyle, Cgstyle) 
 {
-    gstyle = Cgstyle;
-    tstyle = Ctstyle;
     text.setString(text_);
     text.setFont(tstyle->font);
     text.setCharacterSize(tstyle->fontSize);
@@ -13,17 +11,12 @@ GUILabel::GUILabel(RenderWindow* renderWindow_, float x, float y, float width,
     iconT = nullptr;
     icon = nullptr;
     Recalc();
-    // вопро
-    //action = Caction;
-    //clicked = false;
 }
 
 GUILabel::GUILabel(RenderWindow* renderWindow_, Vector2f position_, Vector2f size_,
     std::string text_, TextStyle *Ctstyle, GUIStyle *Cgstyle) :
     GUIBox(renderWindow_, position_, size_, text_, Ctstyle, Cgstyle)
 {
-    gstyle = Cgstyle;
-    tstyle = Ctstyle;
     text.setString(text_);
     text.setFont(tstyle->font);
     text.setCharacterSize(tstyle->fontSize);
@@ -31,34 +24,13 @@ GUILabel::GUILabel(RenderWindow* renderWindow_, Vector2f position_, Vector2f siz
     iconT = nullptr;
     icon = nullptr;
     Recalc();
-    // вопро
-    //action = Caction;
-    //clicked = false;
 }
 
-/*
-GUILabel::GUILabel(RenderWindow * renderWindow_, float x, float y, float width, float height, Texture * icon_, std::string text_, TextStyle *Ctstyle, GUIStyle *Cgstyle) :
-    GUIBox(renderWindow_, x, y, width, height, text_, Ctstyle, Cgstyle)
-{
-    gstyle = Cgstyle;
-    tstyle = Ctstyle;
-    text.setString(text_);
-    text.setFont(tstyle->font);
-    text.setCharacterSize(tstyle->fontSize);
-    text.setFillColor(tstyle->color);
-    Recalc();
-}
-
-GUILabel::GUILabel(RenderWindow * renderWindow_, Vector2f position_, Vector2f size_, Texture * icon_, std::string text_, TextStyle *Ctstyle, GUIStyle *Cgstyle)
-{
-}*/
-
-GUILabel::GUILabel(RenderWindow * renderWindow_, float x, float y, float width, float height, std::string text_, TextStyle * Ctstyle, Texture * icon_, GUIStyle *Cgstyle):
+GUILabel::GUILabel(RenderWindow * renderWindow_, float x, float y, float width, 
+    float height, std::string text_, TextStyle * Ctstyle, Texture * icon_, GUIStyle *Cgstyle):
     GUIBox(renderWindow_, x, y, width, height, text_, Ctstyle, Cgstyle)
 
 {
-    gstyle = Cgstyle;
-    tstyle = Ctstyle;
     text.setString(text_);
     text.setFont(tstyle->font);
     text.setCharacterSize(tstyle->fontSize);
@@ -68,12 +40,11 @@ GUILabel::GUILabel(RenderWindow * renderWindow_, float x, float y, float width, 
     Recalc();
 }
 
-GUILabel::GUILabel(RenderWindow * renderWindow_, Vector2f position_, Vector2f size_, std::string text_, TextStyle * Ctstyle, Texture * icon_, GUIStyle *Cgstyle):
+GUILabel::GUILabel(RenderWindow * renderWindow_, Vector2f position_, Vector2f size_,
+    std::string text_, TextStyle * Ctstyle, Texture * icon_, GUIStyle *Cgstyle):
     GUIBox(renderWindow_, position_, size_, text_, Ctstyle, Cgstyle)
 
 {
-    gstyle = Cgstyle;
-    tstyle = Ctstyle;
     text.setString(text_);
     text.setFont(tstyle->font);
     text.setCharacterSize(tstyle->fontSize);
@@ -85,11 +56,6 @@ GUILabel::GUILabel(RenderWindow * renderWindow_, Vector2f position_, Vector2f si
 
 void GUILabel::Draw()
 {
-    /*renderWindow->draw(sprite);
-    renderWindow->draw(frames[0]);
-    renderWindow->draw(frames[1]);
-    renderWindow->draw(frames[2]);
-    renderWindow->draw(frames[3]);/**/
     if (text.getString() != "")
     {
         renderWindow->draw(text);
@@ -114,7 +80,7 @@ void GUILabel::Recalc()
 
 
     FloatRect textRect = text.getLocalBounds();
-    //
+
     if (icon != nullptr)
     {
         icon->setPosition(posX, posY);
@@ -137,20 +103,4 @@ void GUILabel::Recalc()
     if (tstyle->align == 'l')
         text.setPosition(Vector2f(posX + 1, posY + 1));
     */
-    sprite.setTexture(gstyle->mainTex);
-    sprite.setPosition(posX, posY);
-    sprite.setTextureRect(IntRect(0, 0, width, height));
-    for (int i = 0; i < 4; i++)
-        frames[i].setTexture(gstyle->frame);
-    frames[0].setTextureRect(IntRect(0, 0, gstyle->frameWid, width));
-    frames[1].setTextureRect(IntRect(0, 0, gstyle->frameWid, width));
-    frames[2].setTextureRect(IntRect(0, 0, gstyle->frameWid, height));
-    frames[3].setTextureRect(IntRect(0, 0, gstyle->frameWid, height));
-    frames[0].setPosition(posX, posY + gstyle->frameWid);
-    frames[1].setPosition(posX, posY + height);
-    frames[0].setRotation(-90);
-    frames[1].setRotation(-90);
-    frames[2].setPosition(posX, posY);
-    frames[3].setPosition(posX + width - gstyle->frameWid, posY);
-
 }
--- GUI/Elements/Label/GUILabel.h
+++ GUI/Elements/Label/GUILabel.h
@@ -4,26 +4,43 @@ class GUILabel : public GUIBox
 {
 private:
     friend class GUILayer;
-    GUIStyle *gstyle;
-    TextStyle *tstyle;
-    Sprite sprite;
-    Sprite frames[4];
-    Text text;
-    Texture *iconT;
-    Sprite *icon;
-protected:
+    Text text; /*!< текст */ 
+    Texture *iconT; /*!< текстура иконки */ 
+    Sprite *icon; /*!< иконка, которую можно нарисовать */ 
 
-    // один текст
+protected:
+    //! Конструктор
+    /*!
+    Создает текстовый label
+    */
     GUILabel(RenderWindow* renderWindow_, float x, float y, float width, float height, std::string text, TextStyle *tstyle, GUIStyle *gstyle);
+
+    //! Конструктор
+    /*!
+    Создает текстовый label
+    */
     GUILabel(RenderWindow* renderWindow_, Vector2f position_, Vector2f size_, std::string text, TextStyle *tstyle, GUIStyle *gstyle);
-    //одна картинка
-    /*GUILabel(RenderWindow* renderWindow_, float x, float y, float width, float height, Texture *icon_, std::string text_, TextStyle *tstyle, GUIStyle *gstyle);
-    GUILabel(RenderWindow* renderWindow_, Vector2f position_, Vector2f size_, Texture *icon_, std::string text_, TextStyle *tstyle, GUIStyle *gstyle);*/
-    //картинка + текст (положение?)
+
+    //! Конструктор
+    /*!
+    Создает label, состоящий из одной иконки, если текст задан пустой строкой.
+    Если текст задан, то создает label, содержащий иконку и текст
+    \param renderWindow_ окно, в которое рисуем
+    */
     GUILabel(RenderWindow* renderWindow_, float x, float y, float width, float height, std::string text, TextStyle *tstyle, Texture *icon_, GUIStyle *gstyle);
+    
+    //! Конструктор
+    /*!
+    Создает label, состоящий из одной иконки, если текст задан пустой строкой.
+    Если текст задан, то создает label, содержащий иконку и текст
+    \param renderWindow_ окно, в которое рисуем
+    */
     GUILabel(RenderWindow* renderWindow_, Vector2f position_, Vector2f size_, std::string text, TextStyle *tstyle, Texture *icon_, GUIStyle *gstyle);
     
+    //! Переопределяем функцию Draw
     virtual void Draw() override;
+
+    //! Переопределяем функцию Recalc
     virtual void Recalc() override;
 };
 
