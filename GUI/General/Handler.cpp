09df069cf0190b2d41d73933add674f5271a6cd5
IgorBeschastnov <30983257+IgorBeschastnov@users.noreply.github.com>
2017-11-21 17:32:52 +0300

    worl

--- GUI/General/Handler.cpp
+++ GUI/General/Handler.cpp
@@ -4,6 +4,8 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+
+
 //окей. теперь fun part
 void main() 
 {
@@ -27,14 +29,21 @@ void main()
 	std::shared_ptr<GUILayer> layer = main.CreateGUILayer(Vector2f(0,0), Vector2f(SCREEN_WIDTH, SCREEN_HEIGHT));
 	//вот досюда
 
-	//пример создания просто "коробки" с созданными стилями, положением 0,0 и размером 300,400 на созданном выше слое
-	std::shared_ptr<GUIBox> box = layer->CreateBox(0, 0, 20,10, "omegaLUL", &tst, &gst);
-
 	// Cоздание прогресс бара. 
 	std::shared_ptr<GUIProgressBar> progressBar = layer->CreateProgressBar(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 300, 30, "Sorting...", &tst, &gst,
 		0, 200, Color::White, Color::Green);
+	
+	//Создание label
+	Texture icon, icon2;
+	icon.loadFromFile("Danger.png");
+	icon2.loadFromFile("Galaxy.png");
+	// просто текст
+	std::shared_ptr<GUILabel> label = layer->CreateLabel(0, 300, 0, 0, "Just text", &tst, &gst);
+	// иконка
+	std::shared_ptr<GUILabel> label1 = layer->CreateLabel(0, 330, 0, 0, "", &tst, &icon, &gst);
+	// текст и иконка
+	std::shared_ptr<GUILabel> label2 = layer->CreateLabel(0, 400, 0, 0, "text to the right of the icon", &tst, &icon2, &gst);
 
-	//красиво жи ну, ради этого все и затевалось
 	while (1)
 	{
 		// Костыль для теста на время, пока нет Observer.
