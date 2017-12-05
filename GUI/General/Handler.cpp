b8046e1215d2ce0c2a87555e15065984594411df
IgorBeschastnov <30983257+IgorBeschastnov@users.noreply.github.com>
2017-12-05 22:15:38 +0300

    tmp

--- GUI/General/Handler.cpp
+++ GUI/General/Handler.cpp
@@ -93,7 +93,8 @@ void main()
 	// текст подгружается из локали
     std::shared_ptr<GUIButton> button = layer->CreateButton(20, 60, 100,100, locale.GetElementName("button"), &tst, &gst, &buttonAction);
 	std::shared_ptr<GUICheckButton> checkButton = layer->CreateCheckButton(300, 60, 100, 100, locale.GetElementName("check_button"), &tst, &gst, &buttonAction);
-    // создание label
+    
+	// создание label
     Texture icon;
     icon.loadFromFile("resources/Danger.png");
 
